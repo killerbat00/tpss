f269c4a9474449f5ad5ccb2dec38da5be7570846
brian morrow <bmorrow92@gmail.com>
2013-12-15 19:04:24 -0500

    Style clean up

--- skateapp/views.py
+++ skateapp/views.py
@@ -12,14 +12,6 @@ def teardown_request(exception):
     if db is not None:
         db.close()
 
-@app.route('/')
-def home():
-    return render_template('map.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -27,3 +19,15 @@ def page_not_found(e):
 @app.errorhandler(500)
 def server_error(e):
     return render_teamplte('500.html'), 500
+
+@app.route('/')
+def home():
+    return render_template('map.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
