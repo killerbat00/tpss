a16dd38ad26e790e1acc2635c7838d6620eea658
brian morrow <bmorrow92@gmail.com>
2013-12-16 01:26:47 -0500

    fix misspelling

--- skateapp/views.py
+++ skateapp/views.py
@@ -18,7 +18,7 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def server_error(e):
-    return render_teamplte('500.html'), 500
+    return render_template('500.html'), 500
 
 @app.route('/')
 def home():
